bd7508b1245ca5a42f093a3e77796a724e030214
iamprc4 <pratithi04rani@gmail.com>
2026-04-03 21:58:26 +0530

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './ThemeContext';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import FaceGuard from './pages/FaceGuard';
+import './App.css';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('Guest');
+
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Navigation 
+          isLoggedIn={isLoggedIn} 
+          username={username}
+          onLogin={() => setIsLoggedIn(true)}
+          onLogout={() => setIsLoggedIn(false)}
+          onUsernameChange={setUsername}
+        />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard username={username} />} />
+          <Route path="/face-guard" element={<FaceGuard />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ react/src/ThemeContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useEffect } from 'react';
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const isDark = true;
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.setAttribute('data-theme', 'dark');
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme: () => {} }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
